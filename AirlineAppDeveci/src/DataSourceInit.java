46ca3091676a5e0fa413d8f5f100ee88b73085a6
osman <osman@10.0.0.22>
2017-09-21 23:49:06 +0200

    Sehr viele Änderungen. Vorerst verzichtet auf die DataSourceInit-Klasse, da diese sehr umständlich wäre, stattdessen die DataSource direkt in der Model-Klasse definiert. Datensätze können nun rausgelesen werden (getestet).

--- AirlineAppDeveci/src/DataSourceInit.java
+++ AirlineAppDeveci/src/DataSourceInit.java
@@ -5,19 +5,61 @@ import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 public class DataSourceInit extends MysqlDataSource {
 	
+	String servername;
+	int port;
+	String user;
+	String password;
+	
 	public DataSourceInit(String servername,int port,String user,String password) {
+		this.servername = servername;
+		this.port = port;
+		this.user = user;
+		this.password = password;
+		
 		MysqlDataSource ds = new MysqlDataSource();
-		try {
-			Connection con = ds.getConnection();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
 		ds.setServerName(servername);
 		ds.setPortNumber(port);
 		ds.setUser(user);
 		ds.setPassword(password);
+		System.out.println(ds.getServerName()+ds.getUser());
+		
 	}
+
+	public String getServername() {
+		return servername;
+	}
+
+	public void setServername(String servername) {
+		this.servername = servername;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+
 	
 	
 }
--- AirlineAppDeveci/src/Model.java
+++ AirlineAppDeveci/src/Model.java
@@ -1,36 +1,92 @@
 import java.awt.*;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
 import java.util.Scanner;
 
 import javax.swing.*;
 
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+
+
 public class Model {
-	JTextField userVal = new JTextField("username");
-	JTextField portVal = new JTextField("portnummer");
+	JTextField userVal = new JTextField("root");
+	JTextField portVal = new JTextField("3306");
 	JTextField pwdVal = new JTextField("password");
-	JTextField serverVal = new JTextField("servername");
+	JTextField serverVal = new JTextField("localhost");
+	JTextField dbVal = new JTextField("datenbank");
 	JButton buttonServer = new JButton("bestätigen");
-	DataSourceInit ds;
+	ActionListener l;
 	public void init() {
+		
+		// GUI-Frame Modellierung
+		
 		JFrame jf = new JFrame("AirlineApp");
 		jf.setSize(600, 600);
 		JPanel jp = new JPanel();
-		jp.add(userVal);
-		jp.add(pwdVal);
 		jp.add(serverVal);
 		jp.add(portVal);
+		jp.add(userVal);
+		jp.add(pwdVal);
+		jp.add(dbVal);
 		jp.add(buttonServer);
+		buttonServer.setName("server");
+	
 		jp.setBackground(Color.LIGHT_GRAY);
 		jf.setLocationRelativeTo(null);
 		jf.setContentPane(jp);
+		
+		// Action-Listener für diverse Buttons (unter anderem für non-hardcoded parameters für ds)
+		
+		buttonServer.addActionListener(l);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jf.setVisible(true);
+	}
+	
+	public void dataCheck () {
+		 l = new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				MysqlDataSource ds = new MysqlDataSource();
+				// DataSource Parameter werden gesetzt.
+				ds.setUser(userVal.getText());
+				ds.setPassword(pwdVal.getText());
+				ds.setServerName(serverVal.getText());
+				ds.setPortNumber(Integer.parseInt(portVal.getText()));
+				try {
+					
+					// Driver werden geladen, sagt auf welche DB zugegriffen werden soll, (alternativ geht auch 
+					// in der SELECT query database.tablename zu schreiben)
+					String url = "jdbc:mysql://"+serverVal.getText()+":"
+					+Integer.parseInt(portVal.getText())+"/"+dbVal.getText();
+					Connection con2 = DriverManager.getConnection(url,userVal.getText(),pwdVal.getText());
+					Statement st = con2.createStatement();
+					// Test-Query
+					ResultSet rs = st.executeQuery("SELECT * from airlines");
+					rs.next();
+					String test = rs.getString("name");
+					System.out.println(test);
+					rs.close(); st.close(); con2.close();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+			}
+		};
+		
+		buttonServer.addActionListener(l);
 	}
 	
 	
 	public static void main (String []args) {
 		Model m = new Model();
 		m.init();
+		m.dataCheck();
 	}
 
 	
