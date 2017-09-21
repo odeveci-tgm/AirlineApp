3f34d44a203194ecb6e5c6c102e495d6b4c674ac
osman <osman@10.0.0.22>
2017-09-21 18:19:50 +0200

    Initialisierungs-Commit

---
+++ AirlineAppDeveci/src/Model.java
@@ -0,0 +1,26 @@
+import java.awt.Color;
+
+import javax.swing.*;
+
+public class Model {
+
+	
+	public void init() {
+		JFrame jf = new JFrame("BookingApp");
+		jf.setSize(600, 600);
+		JPanel jp = new JPanel();
+		jp.setBackground(Color.LIGHT_GRAY);
+		jf.setLocationRelativeTo(null);
+		jf.setContentPane(jp);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setVisible(true);
+	}
+	
+	
+	public static void main (String []args) {
+		Model m = new Model();
+		m.init();
+	}
+
+	
+}
