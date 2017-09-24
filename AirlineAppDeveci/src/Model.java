8ff3c5a248790bab2060198c30f090a99d7f6df3
osman <osman@10.0.0.22>
2017-09-24 19:20:12 +0200

    Flüge werden erfolgreich angezeigt. Wenn kein Flug vorhanden ist -> Rückmeldung + alle DropDown-Listen werden leer. Bei einem Connection Fail genauso.

--- AirlineAppDeveci/src/Model.java
+++ AirlineAppDeveci/src/Model.java
@@ -33,18 +33,27 @@ public class Model {
 	JLabel fail = new JLabel("");
 	JComboBox<String> jcDept = new JComboBox();
 	JComboBox<String> jcArv = new JComboBox();
+	JComboBox<String> jcFlights = new JComboBox();
 	JFrame jf = new JFrame("AirlineApp");
 	JPanel jp = new JPanel(new GridBagLayout());
-	JButton buttonServer = new JButton("connect");
+	JButton buttonServer = new JButton("Verbinden");
+	JButton buttonFlight = new JButton("Flüge anzeigen");
 	ActionListener l;
 	
 	
 	public void init() {
 		
 		// GUI-Frame Modellierung
+		userVal.setText("root");
+		portVal.setText("3306");
+		serverVal.setText("localhost");
+		dbVal.setText("list_3ahit");
+		buttonServer.setActionCommand("connect");
+		buttonFlight.setActionCommand("flights");
 		
 		
-		jf.setSize(600, 600);
+		jf.setSize(1000, 700);
+		jf.setResizable(false);
 		
 			// Positionierung der Felder mittels GBC-Layout
 		
@@ -107,21 +116,29 @@ public class Model {
 		c.anchor = GridBagConstraints.EAST;
 		jp.add(jcArv, c);
 		
+		c.gridwidth=6;
+		c.gridy++;
+		c.gridx=0;
+		c.anchor = GridBagConstraints.CENTER;
+		jp.add(buttonFlight,c);
+		
+		c.gridwidth=6;
+		c.gridx=0;
+		c.gridy++;
+		c.anchor = GridBagConstraints.CENTER;
+		jp.add(jcFlights, c);
 		jcArv.setPrototypeDisplayValue("....................................................................");
 		jcDept.setPrototypeDisplayValue(".....................................................................");
-		
-		
-		
+		jcFlights.setPrototypeDisplayValue("...................................................................................................................................................................");
 		jp.setBackground(Color.LIGHT_GRAY);
 		jf.setLocationRelativeTo(null);
 		jf.setContentPane(jp);
 		
 		// Action-Listener für diverse Buttons (unter anderem für non-hardcoded parameters für ds)
+		//buttonServer.addActionListener(l);
 		
-		buttonServer.addActionListener(l);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jf.setVisible(true);
-		jf.pack();
 	}
 	
 	public void dataCheck () {
@@ -130,60 +147,112 @@ public class Model {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				MysqlDataSource ds = new MysqlDataSource();
-				
-			
-				
 				try {
-					// DataSource Parameter werden gesetzt.
-					
-					ds.setUser(userVal.getText());
-					ds.setPassword(pwdVal.getText());
-					ds.setServerName(serverVal.getText());
-					ds.setPortNumber(Integer.parseInt(portVal.getText()));
-					
-					// Driver werden geladen, sagt auf welche DB zugegriffen werden soll, (alternativ geht auch 
-					// in der SELECT query database.tablename zu schreiben)
 					
 					String url = "jdbc:mysql://"+serverVal.getText()+":"
 					+Integer.parseInt(portVal.getText())+"/"+dbVal.getText();
-					Connection con2 = DriverManager.getConnection(url,userVal.getText(),pwdVal.getText());
-					Statement st = con2.createStatement();
+					Connection con = DriverManager.getConnection(url,userVal.getText(),pwdVal.getText());
 					
-					// Query für alle möglichen Flughäfen formatiert in "<airportname>,<landcode>"
+					switch (e.getActionCommand()) {
+						
 					
-					ResultSet rs = st.executeQuery("select name,country from airports");
+					case "connect":
+										// DataSource Parameter werden gesetzt.
+										
+										ds.setUser(userVal.getText());
+										ds.setPassword(pwdVal.getText());
+										ds.setServerName(serverVal.getText());
+										ds.setPortNumber(Integer.parseInt(portVal.getText()));
+										
+										// Driver werden geladen, sagt auf welche DB zugegriffen werden soll, (alternativ geht auch 
+										// in der SELECT query database.tablename zu schreiben)
+										
+										
+										Statement st = con.createStatement();
+										ResultSet rs = st.executeQuery("select countries.name,airports.name from airports,countries WHERE airports.country = countries.code ORDER BY 1;");
+										
+									
+										//ArrayList für alle Elemente
+										
+										ArrayList<String> airports = new ArrayList<String>();
+										while(rs.next()) {
+											String country = rs.getString("countries.name");
+											String airport = rs.getString("airports.name");
+											airports.add(airport+","+country);
+										}
+										
+										
+										//Umwandlung in Array anschließend Zuweisung für die jeweiligen Dropdowns
+										
+										String airArr[] = airports.toArray(new String[airports.size()]);
+										int i = 0;
+										
+										for(String str: airArr) {
+											jcDept.addItem(str);
+											jcArv.addItem(str);
+											i++;
+										}
+										System.out.println(i);
+										
+										fail.setForeground(Color.green);
+										fail.setText("Connection established!");
+										rs.close(); st.close(); con.close();
+										break;
+										
+										
+					case "flights":		
+										// Value von der DropDown wird geholt. Land und Flughafenname werden wieder getrennt per split().
+										// Esentziell für die Query
+										jcFlights.removeAllItems();
+										String dep =  String.valueOf(jcDept.getSelectedItem());
+										String arv = String.valueOf(jcArv.getSelectedItem());
+										String[] partsDep = dep.split(",");
+										String[] arvDep = arv.split(",");
+										System.out.println(partsDep[0]);
+										Statement st2 = con.createStatement();
+										ResultSet rs2 = st2.executeQuery("select * from flights,(select airportcode as 'depcode' from airports "
+										+ "WHERE name='"+partsDep[0]+"')dep,(select airportcode as 'arrcode' from airports WHERE name='"+arvDep[0]+"')arr "
+										+ "WHERE depcode = departure_airport AND arrcode = destination_airport;");
+										
+										ArrayList<String> flights = new ArrayList<String>();
+										while(rs2.next()) {
+												flights.add(rs2.getString("departure_airport")+","+rs2.getTime("departure_time")+"," 
+												+ rs2.getString("destination_airport")+","+rs2.getTime("destination_time"));
+												
+										}
+										
+										String[] flightsArr = flights.toArray(new String[flights.size()]);
+										
+										for (String str:flightsArr) {
+											jcFlights.addItem(str);
+										}
+										
+										if (flightsArr.length==0) {
+											fail.setForeground(Color.red);
+											fail.setText("Keine Flüge vorhanden.");
+										}
+										
+										break;
+										
+					default:			
+										System.out.println("Something went wrong.");
+										break;
+									}
 					
-					//ArrayList für alle Elemente
 					
-					ArrayList<String> airports = new ArrayList<String>();
-					while(rs.next()) {
-						airports.add(rs.getString("name")+","+rs.getString("country"));
-					}
+									
 					
+					// Query für alle möglichen Flughäfen formatiert in "<countryname>,<airportname>"
 					
-					//Umwandlung in Array anschließend Zuweisung für die jeweiligen Dropdowns
-					
-					String airArr[] = airports.toArray(new String[airports.size()]);
-					int i = 0;
-					
-					for(String str: airArr) {
-						jcDept.addItem(str);
-						jcArv.addItem(str);
-						i++;
-					}
-					System.out.println(i);
-					
-					fail.setForeground(Color.green);
-					fail.setText("Connection established!");
-					jf.pack();
-					rs.close(); st.close(); con2.close();
 					
 					
 				} catch (SQLException e1) {
 					// TODO Auto-generated catch block
 					fail.setForeground(Color.red);
 					fail.setText("Connection failed! Überprüfen Sie Ihre Werte.");
-					jf.pack();
+					jcFlights.removeAllItems();
+					jcDept.removeAllItems();
+					jcArv.removeAllItems();
 					e1.printStackTrace();	
 				}
 					// Wird wahrscheinlich nie eintreten
@@ -191,14 +260,20 @@ public class Model {
 					e2.printStackTrace();
 					fail.setText("Geben Sie bitte überall gültige Werte ein!");
 					fail.setForeground(Color.red);
-					jf.pack();
+					jcFlights.removeAllItems();
+					jcDept.removeAllItems();
+					jcArv.removeAllItems();
+					
 				}
 				
 				catch (NumberFormatException e3) {
 					e3.printStackTrace();
 					fail.setText("Ungültige Portnummer oder fehlende Eingaben!");
 					fail.setForeground(Color.red);
-					jf.pack();
+					jcFlights.removeAllItems();
+					jcDept.removeAllItems();
+					jcArv.removeAllItems();
+					
 				}
 				
 				
@@ -206,6 +281,7 @@ public class Model {
 		};
 		
 		buttonServer.addActionListener(l);
+		buttonFlight.addActionListener(l);
 	}
 	
 	
