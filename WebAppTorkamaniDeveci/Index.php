8931f7b151969bbaee913a5d1cadf5253938d957
storkamani-tgm <31983368+storkamani-tgm@users.noreply.github.com>
2017-10-05 19:29:17 +0200

    Add files via upload

---
+++ WebAppTorkamaniDeveci/Index.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		
+		<title>
+			WebApp Torkamani und Deveci A01
+		</title>
+	</head>
+		<body>
+			<br>
+			<br>
+			<br>
+			<h1 align="center">
+				Flugnummer
+			</h1>
+			<br>
+			<h4 align="center">
+			von Osman Deveci und Sami Torkamani
+			</h4>
+			<br>
+			<br>
+			<!-- Osman mit get wird beim rauswerfen eines passagiers die vorherige flugnummer übergeben -->
+			<form action="Fluginformation.php" method="get" align="center">
+				<input type="text" placeholder="Flugnummer bitte eingeben!" name="flugnr">
+				<br>
+				<br>
+				<br>
+				<button type="submit">
+					CLICK ME
+				</button>
+			</form>
+		</body>
+</html>
